490917e8d41f5e504bc27dcfc9e008af66a5de8f
loveless-savage <titansofalgenia@gmail.com>
2019-01-10 16:23:50 -0800

    First Commit

---
+++ src/cpp/Robot.cpp
@@ -0,0 +1,37 @@
+#include "WPILib.h"
+#include <ctre/Phoenix.h>
+#include <iostream>
+
+class Robot: public IterativeRobot{
+
+	//Talons
+
+	//Objects
+
+	//Other Variables
+
+    void RobotInit(){
+    }
+
+    void TeleopInit(){
+    }
+    void TeleopPeriodic(){
+    }
+
+    void AutonomousInit(){
+    }
+    void AutonomousPeriodic(){
+    }
+
+    void TestInit(){
+    }
+    void TestPeriodic(){
+    }
+
+    void DisabledInit(){
+    }
+    void DisabledPeriodic(){
+    }
+
+};
+START_ROBOT_CLASS(Robot);
