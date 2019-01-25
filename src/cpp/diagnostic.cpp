0826262fc7980a53fe6b0b6ae022b75e9b2bed87
loveless-savage <titansofalgenia@gmail.com>
2019-01-24 21:04:58 -0800

    partial diagnostic

--- src/cpp/diagnostic.cpp
+++ src/cpp/diagnostic.cpp
@@ -1,3 +1,20 @@
 #include <diagnostic.h>
 
 
+// increase or decrease ID number
+void Diagnostic :: tickID (bool sign) {
+	if(sign){
+		currentID++;
+	}else{
+		currentID--;
+	}
+}
+
+// change value of current talon
+void Diagnostic :: writeToCurrentTalon (float speed) {
+	talonCatalog[currentID]->Set(ControlMode::PercentOutput, speed );
+}
+
+// public handler for all diagnostic operations
+void Diagnostic :: update () {
+}
--- src/include/diagnostic.h
+++ src/include/diagnostic.h
@@ -5,10 +5,50 @@
 #include <ctre/Phoenix.h>
 #include <iostream>
 #include <constants.h>
+#include <typeinfo> // we may need this for type differentiation
 
 
 class Diagnostic {
+private:
+	void tickID(bool sign);
+	void writeToCurrentTalon(float speed);
+
+	int currentID=0;
+	TalonSRX* talonCatalog[10]={};
+	Joystick *joy;
+
 public:
+	void update();
+
+	// initialize diagnostic
+	Diagnostic (
+		// receive parameters
+		Joystick *joy,
+		TalonSRX *drivebase_left_enc,
+		TalonSRX *drivebase_left_noenc,
+		TalonSRX *drivebase_right_enc,
+		TalonSRX *drivebase_right_noenc,
+		TalonSRX *intake_wheels,
+		TalonSRX *intake_clamp,
+		TalonSRX *elevator_enc,
+		TalonSRX *elevator_noenc
+	):
+		// initialize member variables
+		joy(joy),// joystick
+		talonCatalog({// all the talons arranged in an array
+			drivebase_left_enc,
+            drivebase_left_noenc,
+            drivebase_right_enc,
+            drivebase_right_noenc,
+            intake_wheels,
+            intake_clamp,
+            elevator_enc,
+            elevator_noenc
+		})
+	{
+		// run on initialization
+		std::cout<<"initializing diagnostic";
+	};
 };
 
 #endif
